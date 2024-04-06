2a0bf5e29af8c9ace7da0e0714616c87e7bcc978
zhaomsdemo <zhaomsdemo@gmail.com>
2024-04-05 22:24:22 -0700

    Added Security config

--- src/main/java/com/zhaomsdemo/customertransaction/CustomerTransactionApplication.java
+++ src/main/java/com/zhaomsdemo/customertransaction/CustomerTransactionApplication.java
@@ -2,9 +2,11 @@ package com.zhaomsdemo.customertransaction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 @EnableMongoRepositories
 public class CustomerTransactionApplication {
 
--- src/main/java/com/zhaomsdemo/customertransaction/config/SecurityConfig.java
+++ src/main/java/com/zhaomsdemo/customertransaction/config/SecurityConfig.java
@@ -2,10 +2,14 @@ package com.zhaomsdemo.customertransaction.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity(securedEnabled = true)
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
