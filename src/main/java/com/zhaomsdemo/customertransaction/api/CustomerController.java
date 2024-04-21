8b7081ed51235c74340e4bc5000a50973e71c406
zhaomsdemo <zhaomsdemo@gmail.com>
2024-04-20 22:05:04 -0700

    add zipkin

--- src/main/java/com/zhaomsdemo/customertransaction/api/CustomerController.java
+++ src/main/java/com/zhaomsdemo/customertransaction/api/CustomerController.java
@@ -4,25 +4,30 @@ import com.zhaomsdemo.customertransaction.dto.CustomerDto;
 import com.zhaomsdemo.customertransaction.entity.Customer;
 import com.zhaomsdemo.customertransaction.service.CustomerService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/customers")
 @AllArgsConstructor
+@Slf4j
 public class CustomerController {
 
     private final CustomerService customerService;
 
     @PostMapping("/")
     public ResponseEntity<Customer> createNewCustomer(@RequestBody CustomerDto customerDto) {
+        log.info("Creating a new customer: {}", customerDto);
         Customer newCustomer = customerService.saveCustomer(customerDto);
         return ResponseEntity.ok(newCustomer);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Customer> getCustomerById(@PathVariable String id) {
+        log.info("Getting customer by id: {}", id);
         Customer customer = customerService.getCustomerById(id);
+        log.info("Getting customer by id: {}", customer);
         return ResponseEntity.ok(customer);
     }
 }
